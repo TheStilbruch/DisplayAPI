5e2d20a4c2a8ee8ca2cff66810982831aac5e87f
ElDoggo <spookyFalco@gmail.com>
2017-05-01 19:55:35 -0500

    Changed .gitignore and .gitattributes files

--- src/com/eldoggo/displayapi/nms/NMS.java
+++ src/com/eldoggo/displayapi/nms/NMS.java
@@ -4,23 +4,23 @@ import org.bukkit.entity.Player;
 
 public interface NMS {
 
-	public void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut);
+	void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut);
 	
-	public void broadcastTitle(String title, String subtitle, int fadeIn, int stay, int fadeOut);
+	void broadcastTitle(String title, String subtitle, int fadeIn, int stay, int fadeOut);
 	
-	public void sendTitleReset(Player player);
+	void sendTitleReset(Player player);
 	
-	public void broadcastTitleReset();
+	void broadcastTitleReset();
 	
-	public void sendActionMessage(Player player, String message);
+	void sendActionMessage(Player player, String message);
 	
-	public void broadcastActionMessage(String message);
+	void broadcastActionMessage(String message);
 
-	public void sendTabHeaderFooter(Player player, String header, String footer);
+	void sendTabHeaderFooter(Player player, String header, String footer);
 	
-	public void broadcastTabHeaderFooter(String header, String footer);
+	void broadcastTabHeaderFooter(String header, String footer);
 	
-	public void sendTabHeaderFooterReset(Player player);
+	void sendTabHeaderFooterReset(Player player);
 	
-	public void broadcastTabHeaderFooterReset();
+	void broadcastTabHeaderFooterReset();
 }
